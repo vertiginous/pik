919f59ce1da5387fc5de8abf60fa8a830585cd72
Gordon Thiesfeld <gthiesfeld@gmail.com>
2009-06-03 12:19:17 -0500

    added bin/pik.bat to repo.  used only for testing, not part of the gem

---
+++ bin/pik.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"ruby.exe" "c:/ruby/186-p287-mswin32/bin/pik" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"ruby.exe" -rubygems "%~dpn0" %*
+CALL "C:\Documents and Settings\gthiesfeld\.pik\pik.bat"
