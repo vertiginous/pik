ce168132c870c7a99d990feb784ddb164901f853
Gordon Thiesfeld <gthiesfeld@gmail.com>
2009-09-04 13:25:46 -0500

    change remove 'force' to 'quiet'

--- lib/pik/commands/remove_command.rb
+++ lib/pik/commands/remove_command.rb
@@ -6,20 +6,20 @@ module Pik
     it "Removes a ruby location from pik."
     include ConfigFileEditor
     
-    attr_reader :force
+    attr_reader :quiet
     
     def execute
       to_remove = self.class.choose_from(@args, @config)
       raise QuitError unless to_remove
-      if force || @hl.agree("Are you sure you'd like to remove '#{to_remove}'? [Yn] ")  
+      if quiet || @hl.agree("Are you sure you'd like to remove '#{to_remove}'? [Yn] ")  
         @config.delete(to_remove)
-        @hl.say("#{to_remove} removed")
+        @hl.say("#{to_remove} removed") unless quiet
       end
     end
     
     def command_options
-      options.on("--force", "-f", "Remove without prompting") do |value|
-        @force = value
+      options.on("--quiet", "-q", "Remove without prompting") do |value|
+        @quiet = value
       end
     end
     
--- spec/remove_command_spec.rb
+++ spec/remove_command_spec.rb
@@ -4,13 +4,16 @@ describe Pik::Remove do
     Pik::Remove.summary.should eql("Removes a ruby location from pik.")
   end
   
-  it "should have a force option" do
-    cmd = Pik::Remove.new(['-f'])
-    cmd.force.should be_true
+  it "should have a 'quiet' option" do
+    cmd = Pik::Remove.new(['--quiet'])
+    cmd.quiet.should be_true
+    
+    cmd = Pik::Remove.new(['-q'])
+    cmd.quiet.should be_true
   end
   
   it "should remove items from the config" do
-    cmd = Pik::Remove.new(['a', '-f'], { 
+    cmd = Pik::Remove.new(['a', '-q'], { 
                                    'a' => {:path => 'C:/ruby/bin', :gem_home => 'C:/users/rupert/.gems'}, 
                                    'b' => {:path => 'C:/Ruby19/bin'}
                                    }
