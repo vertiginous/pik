4d2e32b6cf366c91704440d35f2e0bc91b6ee3ad
Gordon Thiesfeld <gthiesfeld@gmail.com>
2009-09-19 16:26:19 -0500

    switch_command help docs

--- lib/pik/commands/switch_command.rb
+++ lib/pik/commands/switch_command.rb
@@ -25,20 +25,20 @@ module Pik
       #   @gem_home = value
       # end
       sep =<<SEP
-Switches ruby versions based on a pattern.
+Switches ruby versions based on patterns.
   
     Examples:
 
-      C:\>pik sw 186 mingw
+      C:\\>pik sw 186 mingw
        == Switching to ruby 1.8.6 (2009-03-31 patchlevel 368) [i386-mingw32] ==
       
-      C:\>pik switch 191 p1
+      C:\\>pik switch 191 p1
        == Switching to ruby 1.9.1p129 (2009-05-12 revision 23412) [i386-mingw32] ==
       
-      C:\>pik use 186 mswin
+      C:\\>pik use 186 mswin
        == Switching to ruby 1.8.6 (2008-08-11 patchlevel 287) [i386-mswin32] ==
       
-      C:\>pik 191 p2
+      C:\\>pik 191 p2
        == Switching to ruby 1.9.1p243 (2009-07-16 revision 24175) [i386-mingw32] ==
 SEP
       options.separator sep  
