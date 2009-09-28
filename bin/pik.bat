2c0defdb97e82378711443b313f98b3cad44300a
Gordon Thiesfeld <gthiesfeld@gmail.com>
2009-09-28 13:05:09 -0500

    initial ir support, plus some changes to make testing easier

--- bin/pik.bat
+++ bin/pik.bat
@@ -3,4 +3,4 @@ IF NOT "%~f0" == "~f0" GOTO :WinNT
 @"ruby.exe" "c:/ruby/186-p287-mswin32/bin/pik" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
-@"ruby.exe" -rubygems "%~dpn0" %*
+@"ruby.exe" "%~dpn0" %*
