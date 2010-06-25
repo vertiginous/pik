c385eea46c68f33c73aaa9bdb93ee7919029375d
Gordon Thiesfeld <gthiesfeld@gmail.com>
2010-06-25 06:30:08 -0500

    use absolute path for test script

--- tools/pik_ruby.cmd
+++ tools/pik_ruby.cmd
@@ -1,4 +1,4 @@
 :: This is a pik launcher that runs in ruby to test changes without the compile loop.
 @ECHO OFF
-ruby "%~dp0pik_runner" "%~f0" %*
+C:\Ruby\ruby-187-p249\bin\ruby.exe "%~dp0pik_runner" "%~f0" %*
 IF EXIST "%USERPROFILE%\.pik\pik.bat" (call "%USERPROFILE%\.pik\pik.bat")
