1a3a9152e2ca28924871feba034df4599f5a9047
Gordon Thiesfeld <gthiesfeld@gmail.com>
2011-01-27 11:12:55 -0600

    changed devkit url

--- lib/pik/packages.rb
+++ lib/pik/packages.rb
@@ -11,7 +11,7 @@ module Pik
 
     PACKAGES = {
       "sqlite"=>{:url=>"http://www.sqlite.org/sqlite-dll-win32-x86-3070400.zip"},
-      "devkit"=>{:url=>"http://cdn.rubyinstaller.org/archives/devkits/DevKit-tdm-32-4.5.1-20101231-1457.7z"},
+      "devkit"=>{:url=>"http://cdn.rubyinstaller.org/archives/devkits/DevKit-tdm-32-4.5.1-20101214-1400-sfx.exe"},
       "7zip"=>{:url=>"http://downloads.sourceforge.net/sevenzip/7za920.zip"},
       "ansicon"=>{:url=>"http://adoxa.110mb.com/ansicon/ansi132.zip"}
     }
